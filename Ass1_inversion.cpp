96d9aa006d2f102cea0b47071e4065794bc8ec14
Pruthvi <Pruthvip15@gmail.com>
2015-07-02 17:44:15 +0530

    i/o in c++

--- Ass1_inversion.cpp
+++ Ass1_inversion.cpp
@@ -1,8 +1,28 @@
+
+#include<iostream.h>
 #include<stdio.h>
+#include<fstream.h>
 //#include<conio.h>
+#define max 100
+
+
+int arr[1
 
 int main()
 {
+    
+    ifstream inFile;
+    int number;
+    inFile.open("IntegerArray.txt",ios::in);
+    if(inFile)
+    {while(inFile){
+              inFile>>number;
+              cout<<number<<"\n";
+              }
+    }
+    
+    
+    
 
   //getch();
  return 0;
